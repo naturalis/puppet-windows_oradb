704d6e0e7617c4ff6ecb19dd6986dd2d62f2ceda
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2014-06-28 14:12:09 +0200

    Modifications

--- manifests/database.pp
+++ manifests/database.pp
@@ -21,16 +21,18 @@ define windows_oradb::database ( # General
   file { "${installFolder}/dbca_${title}.rsp":
     ensure  => present,
     content => template("windows_oradb/dbca_${version}.rsp.erb"),
+    source_permissions => ignore,
   }
 
 # Copy template file
   file { "${installFolder}/${templateName}":
     ensure => present,
     source => "puppet:///modules/windows_oradb/${templateName}",
+    source_permissions => ignore,
   }
 
   exec { "Install database ${title}":
-    command => "dbca.exe -silent -responseFile ${installFolder}\\dbca_${title}.rsp",
+    command => "dbca -silent -responseFile ${installFolder}\\dbca_${title}.rsp",
     path => '${oracleHome}/bin',
     #creates => $oracleHome,
     require => File["${installFolder}/dbca_${title}.rsp"],
