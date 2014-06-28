9e10e274ad84f45279e4fcd184ea268de0459de1
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2014-06-28 14:18:56 +0200

    Modifications

--- manifests/database.pp
+++ manifests/database.pp
@@ -13,29 +13,32 @@ define windows_oradb::database ( # General
                                  $systemPassword = undef,
                                ) {
 
+  notice($oracleHome)
+
   if (!( $version == '11.2.0.3')){
     fail("Unrecognized database install version, use 11.2.0.3")
   }
 
 # Create response file
   file { "${installFolder}/dbca_${title}.rsp":
-    ensure  => present,
-    content => template("windows_oradb/dbca_${version}.rsp.erb"),
+    ensure             => present,
+    content            => template("windows_oradb/dbca_${version}.rsp.erb"),
     source_permissions => ignore,
   }
 
 # Copy template file
   file { "${installFolder}/${templateName}":
-    ensure => present,
-    source => "puppet:///modules/windows_oradb/${templateName}",
+    ensure             => present,
+    source             => "puppet:///modules/windows_oradb/${templateName}",
     source_permissions => ignore,
   }
 
   exec { "Install database ${title}":
-    command => "dbca -silent -responseFile ${installFolder}\\dbca_${title}.rsp",
-    path => '${oracleHome}/bin',
+    command  => "dbca -silent -responseFile ${installFolder}\\dbca_${title}.rsp",
+    path     => ${oracleHome}/bin
+    cwd      => $oracleHome,
     #creates => $oracleHome,
-    require => File["${installFolder}/dbca_${title}.rsp"],
+    require  => File["${installFolder}/dbca_${title}.rsp"],
   }
 
 }
