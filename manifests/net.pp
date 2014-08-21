1dc72e39e10a211546efd4243b84d297c370a4a4
rudibroekhuizen <rudi.broekhuizen@naturalis.nl>
2014-08-21 15:00:20 +0200

    Update net.pp

--- manifests/net.pp
+++ manifests/net.pp
@@ -1,17 +1,17 @@
 # == Define: windows_oradb::net
 #
 define windows_oradb::net (
+
 # General
-  $oracleHome     = undef,
-  $oracleBase     = undef,
-  $version        = undef,
-  $installFolder  = undef,
+  $oracleHome    = undef,
+  $oracleBase    = undef,
+  $version       = undef,
+  $installFolder = undef,
+
 # Reponsefile
-  $globalDbName   = undef,
-  $dbName         = undef,
-  $templateName   = undef,
-  $sysPassword    = undef,
-  $systemPassword = undef,
+  #$listenerNumber    = undef,
+  #$listenerNames     = undef,
+  #$listenerProtocols = undef,
   ) {
 
   if (!( $version == '11.2.0.3')){
