45140b9b2104aae031d42d5fd551007e84d43f63
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-05-29 13:11:05 +0200

    Update database.pp

--- manifests/database.pp
+++ manifests/database.pp
@@ -1,47 +1,7 @@
-# == Define: windows_oradb::database
+# == Class: windows_oradb::database
 #
-define windows_oradb::database ( 
-# General
-  $oracleHome     = undef,
-  $oracleBase     = undef,
-  $version        = undef,
-  $installFolder  = undef,
-# Reponsefile
-  $globalDbName   = undef,
-  $dbName         = undef,
-  $templateName   = undef,
-  $sysPassword    = undef,
-  $systemPassword = undef,
-  ) {
-
-  if (!( $version == '11.2.0.3')){
-    fail("Unrecognized database install version, use 11.2.0.3")
-  }
-
-# Create response file
-  file { "${installFolder}/dbca_${title}.rsp":
-    ensure             => present,
-    content            => template("windows_oradb/dbca_${version}.rsp.erb"),
-    source_permissions => ignore,
-  }
-
-# Copy templatefile to templates directory
-  file { "${oracleHome}/assistants/dbca/templates/${templateName}":
-    ensure             => present,
-    source             => "puppet:///modules/windows_oradb/${templateName}",
-    source_permissions => ignore,
-  } 
-  
-# Execute dbca command
-  exec { "Create database ${title}":
-    command   => "cmd.exe /c \"$oracleHome\\BIN\\dbca -silent -responseFile C:\\Install\\dbca_$title.rsp\"",
-    path      => $::path,
-    unless    => "cmd.exe /c \"dir $oracleBase\\admin\\$dbName\"",
-    require   => [File["${installFolder}/dbca_${title}.rsp"],
-                  File["${oracleHome}/assistants/dbca/templates/${templateName}"]],
-    creates   => "$oracleBase/admin/$dbName",
-    logoutput => true,
-    timeout   => 0,
-  }
-
+class windows_oradb::database (
+  $database_hash,
+  ){
+  create_resources('windows_oradb::defines::database', $database_hash)
 }
