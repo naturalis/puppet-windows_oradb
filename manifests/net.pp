15ed8fe16aa82c9f186b9eba535a562269c1a5e0
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-05-29 13:12:58 +0200

    Update net.pp

--- manifests/net.pp
+++ manifests/net.pp
@@ -1,46 +1,8 @@
-# == Define: windows_oradb::net
+# == Class: windows_oradb::net
 #
-define windows_oradb::net (
-
-# General
-  $oracleHome    = undef,
-  $oracleBase    = undef,
-  $version       = undef,
-  $installFolder = undef,
-
-# Reponsefile
-  #$listenerNumber   = undef,
-  #$listenerNames    = undef,
-  $installType       = undef,
-  $listenerProtocols = undef,
-  $nsnProtocol       = undef,
-  $nsnPort           = undef,
-  
+class windows_oradb::net (
+  $net_hash,
   ) {
-
-  if (!( $version == '11.2.0.3')){
-    fail("Unrecognized database install version, use 11.2.0.3")
-  }
-
-# Create response file
-  file { "${installFolder}/netca_${title}.rsp":
-    ensure             => present,
-    content            => template("windows_oradb/netca_${version}.rsp.erb"),
-    source_permissions => ignore,
-  }
-
-# Execute netca command
-  exec { "Install Oracle net ${title}":
-    command   => "cmd.exe /c \"$oracleHome\\BIN\\netca -silent -responseFile C:\\Install\\netca_$title.rsp\"",
-    path      => $::path,
-    creates   => "$oracleHome/network/admin/listener.ora",
-    require   => File["${installFolder}/netca_${title}.rsp"],
-    logoutput => true,
-    timeout   => 0,
-  }
-  
-  notify {"Register listener in database: sqlplus> alter system set local_listener=${title} scope=both;": 
-  }
-
+  create_resources('windows_oradb::defines::net', $net_hash)
 
 }
